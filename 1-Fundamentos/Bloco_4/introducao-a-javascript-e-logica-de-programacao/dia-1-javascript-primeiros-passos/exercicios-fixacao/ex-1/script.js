cd25e3e169346a41c5044204d56e0a4e16fc034f
Kauan <kauan.s.leite@hotmail.com>
2022-04-06 15:58:00 -0300

    Inicio Dia 4.1

---
+++ 1-Fundamentos/Bloco_4/introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-fixacao/ex-1/script.js
@@ -0,0 +1,12 @@
+const myName = "Kauan";
+const birthCity = "Santa Izabel";
+let birthYear = 2004;
+
+// Constante(const) não pode ter o seu valor alterado posteriormente
+// birthCity = "Itaquaquecetuba";
+
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
