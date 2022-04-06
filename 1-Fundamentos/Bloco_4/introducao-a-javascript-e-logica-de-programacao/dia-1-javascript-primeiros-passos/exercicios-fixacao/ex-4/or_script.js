41a531252e2e0a9715d6688084bc07e28535434b
Kauan <kauan.s.leite@hotmail.com>
2022-04-06 18:09:23 -0300

    Fim dos Exercicios de Fixação

---
+++ 1-Fundamentos/Bloco_4/introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-fixacao/ex-4/or_script.js
@@ -0,0 +1,22 @@
+// const bebidaPrincipal =  'cafezinho';
+// const bebidaAlternativa = 'suco de laranja';
+
+// if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+//   console.log("Obrigado por me atender :D")
+// } else {
+//   console.log("Ei, eu não pedi isso!");
+// }
+
+// console.log(true || true); // true
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(false || false); // false
+
+let weekDay = "sabado";
+
+if(weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira" ){
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+else if(weekDay === "sabado" || weekDay === "domingo"){
+    console.log("FINALMENTE, descanso merecido UwU")
+}
