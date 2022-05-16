af0e92f08baf60b3782d600c5304f66233bc8398
Kauan <kauan.s.leite@hotmail.com>
2022-05-16 15:59:53 -0300

    Dia 8.2

---
+++ 1-Fundamentos/Bloco_8/introducao-a-higher-order-function/dia-2-js-es6-hofs-foreach-find-some-every-sort/exercicios-fixacao/ex-8.js
@@ -0,0 +1,13 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+
+people.sort((pessoaA, pessoaB) => pessoaB.age - pessoaA.age);
+
+console.log(people);
