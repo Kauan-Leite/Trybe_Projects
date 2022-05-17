6ea9c730672742abc29b3f2948c422f95f912c3c
Kauan <kauan.s.leite@hotmail.com>
2022-05-17 19:04:56 -0300

    Exercicios 8.3

---
+++ 1-Fundamentos/Bloco_8/introducao-a-higher-order-function/dia-3-js-es6-hofs-map-e-filter/exemplos/map.js
@@ -0,0 +1,27 @@
+//  Maneira mais Trabalhosa sem o MAP
+// const persons = [
+//   { firstName: 'Maria', lastName: 'Ferreira' },
+//   { firstName: 'João', lastName: 'Silva' },
+//   { firstName: 'Antonio', lastName: 'Cabral' },
+// ];
+
+// const fullNames = [];
+
+// for (let index = 0; index < persons.length; index += 1) {
+//   fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+// }
+
+// console.log(fullNames);
+
+
+//  Maneira mais Pratica com o MAP
+
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
