449592a62cbce5a7d09d75283f36a349f3a4ac57
Kauan <kauan.s.leite@hotmail.com>
2022-05-19 16:28:50 -0300

    Gitgnore

---
+++ 1-Fundamentos/Bloco_8/introducao-a-higher-order-function/dia-5-js-es6-hofs-spread-operator-parametro-rest-destructuring-e-mais/exercicios-fixacao/spread.js
@@ -0,0 +1,12 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Manga', 'Banana', 'Maçã'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Leite Condensado', 'Suco de Laranja', 'Mamão'];
+
+const fruitSalad = (fruit, additional) => {
+  // Esreva sua função aqui
+  return ingredientsList = [...fruit, ...additional]
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
